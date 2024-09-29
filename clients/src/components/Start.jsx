e68ff2560de83cd1e9ec02e570f25a607b9974ca
sahilyadav-1 <sahil2804yadav@gmail.com>
2024-09-29 18:39:33 +0530

    Deploy

--- clients/src/components/Start.jsx
+++ clients/src/components/Start.jsx
@@ -20,7 +20,7 @@ function Start() {
   return (
 
 
-    <div className='bg-[#fff] flex items-center justify-center w-[100vw] h-[100vh] flex items-center flex-col -gap-y-10'>
+    <div className='bg-[#fff] flex items-center justify-center w-[100vw] h-[100vh] flex-col -gap-y-10'>
       <lottie-player src="https://assets1.lottiefiles.com/private_files/lf30_kanwuonz.json" background="transparent" speed="1" style={{ width: "300px", height: "260px" }} loop autoplay></lottie-player>
       <h3 className='font-semibold text-[13px] tracking-wider relative -top-16'>Please Wait. It Might take some time</h3>
     </div>
